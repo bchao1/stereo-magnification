ef928df4a2128fac8cfb819c9f44c548846d8d77
傅譽的MacBook <b08901039@ntu.edu.tw>
2022-01-11 22:22:48 +0800

    continue train

--- train.sh
+++ train.sh
@@ -2,4 +2,5 @@ python3 train.py \
     --checkpoint_dir=checkpoints \
     --cameras_glob=../../mnt/MPI/camera_metadata/train/????????????????.txt \
     --image_dir=../../mnt/MPI/images/train \
-    --experiment_name=mpi_depth_learning_model
+    --experiment_name=mpi_depth_learning_model \
+    --continue_train=True
