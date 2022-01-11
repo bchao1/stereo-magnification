ac6227770ac25a9bc0d3503804546063484e74af
傅譽的MacBook <b08901039@ntu.edu.tw>
2022-01-11 23:05:07 +0800

    save model

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ python3 train.py \
     --cameras_glob=../../mnt/MPI/camera_metadata/train/????????????????.txt \
     --image_dir=../../mnt/MPI/images/train \
     --experiment_name=mpi_depth_learning_model \
-    --continue_train=True
+    --continue_train=True \
     --save_latest_freq=100
